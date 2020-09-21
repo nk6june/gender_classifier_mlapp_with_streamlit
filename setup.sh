4bc26f0aa4991d29bab24e4c534435474ad961bd
Naresh Kumar <42600970+nk6june@users.noreply.github.com>
2020-09-21 17:19:27 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"naresh.datascience@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
